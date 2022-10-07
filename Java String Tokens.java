4afd86bc625dae0cb849e8f45a23b0b9583cb8c7
Samar Mursi <samarmursi0@gmail.com>
2022-10-07 13:04:29 +0200

    Solving Java String Tokens

---
+++ Java String Tokens.java
@@ -0,0 +1,40 @@
+/*
+Given a string, , matching the regular expression [A-Za-z !,?._'@]+, split the string into tokens. We define a token to be one or more consecutive English alphabetic letters. Then, print the number of tokens, followed by each token on a new line.
+
+Note: You may find the String.split method helpful in completing this challenge.
+
+Input Format
+
+A single string, .
+
+Constraints
+
+ is composed of any of the following: English alphabetic letters, blank spaces, exclamation points (!), commas (,), question marks (?), periods (.), underscores (_), apostrophes ('), and at symbols (@).
+Output Format
+
+On the first line, print an integer, , denoting the number of tokens in string  (they do not need to be unique). Next, print each of the  tokens on a new line in the same order as they appear in input string .
+*/
+
+//==================================== answer =================================
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        // Write your code here.
+        String[] split = s.split("[!,?._'@' ]+");
+        int x=  split.length;
+      
+      System.out.println(x);
+        for (String string : split) {
+            System.out.println(string);
+        }
+      
+        scan.close();
+    }
+}
+
